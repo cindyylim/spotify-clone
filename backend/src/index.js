638ddf3e1c88dec02ea0d951cbbcee5d6efcbffb
Cindy <cindyl23@proton.me>
2025-04-30 20:59:16 -0700

    Add admin routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,16 +7,28 @@ import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statRoutes from "./routes/stat.route.js";
 import { connectDB } from "./lib/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
-
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB max file size
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -25,7 +37,11 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+app.use((err, req, res, next) => {
+    res.status(500).json({message: "Internal server error", error: err.message});
+});
+
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-    connectDB();
-})
+  console.log("Server is running on port " + PORT);
+  connectDB();
+});
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,14 @@
-import {Router} from "express";
-import { getAdmin } from "../controller/admin.controller.js";
+import { Router } from "express";
+import { createSong, deleteSong, createAlbum, deleteAlbum } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
+router.use(protectRoute, requireAdmin);
+router.get("/check", checkAdmin);
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
 
-router.get("/", getAdmin);
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
